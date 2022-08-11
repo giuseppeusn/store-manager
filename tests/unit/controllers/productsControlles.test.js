48570be10d1c62274cb59ed5a3511e7a2d5607e3
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-11 20:31:30 -0300

    feat: create controller test for GET /products/:id

--- tests/unit/controllers/productsControlles.test.js
+++ tests/unit/controllers/productsControlles.test.js
@@ -3,32 +3,102 @@ const { expect } = require("chai");
 const productsService = require("../../../services/productsService");
 const productsController = require("../../../controllers/productsController");
 
-describe("Get a list of products - Controller", () => {
-  const response = {};
-  const request = {};
-  const result = [
-    { id: 1, name: 'example_name' },
-    { id: 2, name: 'example_name2' }
-  ];
+describe('Products Controller tests', () => {
+  describe('Get a list of products', () => {
+    const response = {};
+    const request = {};
+    const result = [
+      { id: 1, name: 'example_name' },
+      { id: 2, name: 'example_name2' }
+    ];
 
-  before(() => {
-    request.body = {};
+    before(() => {
+      request.body = {};
 
-    response.status = sinon.stub()
-      .returns(response);
-    response.json = sinon.stub()
-      .returns();
-    
-    sinon.stub(productsService, 'getAllProducts').resolves(result);
+      response.status = sinon.stub()
+        .returns(response);
+      response.json = sinon.stub()
+        .returns();
+      
+      sinon.stub(productsService, 'getAllProducts').resolves(result);
+    });
+
+    after(() => {
+      productsService.getAllProducts.restore();
+    });
+
+    it('should response with status 200', async () => {
+      await productsController.getAllProducts(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
   });
 
-  after(() => {
-    productsService.getAllProducts.restore();
-  });
+  describe('Get product by ID', () => {
+    const response = {};
+    const request = {};
 
-  it("should response with status 200", async () => {
-    await productsController.getAllProducts(request, response);
+    describe('In case of success', () => {
+      const result = {
+        code: 200,
+        response: {
+          id: '1',
+          name: 'example_name'
+        }
+      };
 
-    expect(response.status.calledWith(200)).to.be.equal(true);
+      before(() => {
+        request.body = {};
+        request.params = '1';
+
+        response.status = sinon.stub()
+          .returns(response);
+        response.json = sinon.stub()
+          .returns();
+        
+        sinon.stub(productsService, 'getProduct').resolves(result);
+      });
+
+      after(() => {
+        productsService.getProduct.restore();
+      });
+
+      it('should response with status 200 and json with "id" and "name"', async () => {
+        await productsController.getProduct(request, response);
+
+        expect(response.status.calledWith(result.code)).to.be.equal(true);
+        expect(response.status.calledWith(result.response)).to.be.equal(true);
+      });
+    });
+
+    describe('In case of failing', () => {
+      const result = {
+        code: 404,
+        message: 'Product not found'
+      };
+
+      before(() => {
+        request.body = {};
+        request.params = '1';
+
+        response.status = sinon.stub()
+          .returns(response);
+        response.json = sinon.stub()
+          .returns();
+        
+        sinon.stub(productsService, 'getProduct').resolves(result);
+      });
+
+      after(() => {
+        productsService.getProduct.restore();
+      });
+
+      it('should response with status 404 and message "Product not found"', async () => {
+        await productsController.getProduct(request, response);
+
+        expect(response.status.calledWith(result.code)).to.be.equal(true);
+        expect(response.status.calledWith(result.response)).to.be.equal(true);
+      });
+    });
   });
 });

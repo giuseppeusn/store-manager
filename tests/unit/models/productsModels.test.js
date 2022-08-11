3a86dc942398d837917a11510ebfea4b0b57894d
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-11 19:50:33 -0300

    feat: create model test for POST /products

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -3,50 +3,99 @@ const { expect } = require("chai");
 const productsModel = require("../../../models/productsModel");
 const connection = require("../../../models/connection");
 
-describe('Get a list of products - Model', () => {
-  before(() => {
-    const execute = [
-      { id: 1, name: 'example_name' },
-      { id: 2, name: 'example_name2' }
-    ]
+describe("Products Model tests", () => {
+  describe("Get a list of products - Model", () => {
+    before(() => {
+      const execute = [
+        { id: 1, name: "example_name" },
+        { id: 2, name: "example_name2" },
+      ];
 
-    sinon.stub(connection, 'execute').resolves(execute);
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('should return a list of products with "id" and "name"', async () => {
+      const response = await productsModel.getAllProducts();
+
+      expect(response).to.be.a("object");
+      expect(response).to.have.a.property("id");
+      expect(response).to.have.a.property("name");
+    });
   });
 
-  after(() => {
-    connection.execute.restore();
+  describe("Get products by ID - Model", async () => {
+    before(() => {
+      const execute = [
+        {
+          id: 1,
+          name: "example_name",
+        },
+      ];
+
+      sinon.stub(connection, "execute").resolves(execute);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('should return a product with "id" and "name"', async () => {
+      const response = await productsModel.getProduct("1");
+      console.log(response);
+
+      expect(response).to.be.a("object");
+      expect(response).to.have.a.property("id");
+      expect(response).to.have.a.property("name");
+    });
   });
 
-  it('should return a list of products with "id" and "name"', async () => {
-    const response = await productsModel.getAllProducts();
+  describe("Get products by ID - Model", async () => {
+    before(() => {
+      const execute = [
+        {
+          id: 1,
+          name: "example_name",
+        },
+      ];
 
-    expect(response).to.be.a("object");
-    expect(response).to.have.a.property('id');
-    expect(response).to.have.a.property('name');
+      sinon.stub(connection, "execute").resolves(execute);
+    });
 
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('should return a product with "id" and "name"', async () => {
+      const response = await productsModel.getProduct("1");
+      console.log(response);
+
+      expect(response).to.be.a("object");
+      expect(response).to.have.a.property("id");
+      expect(response).to.have.a.property("name");
+    });
   });
-});
 
-describe('Get products by ID - Model', async () => {
-  before(() => {
-    const execute = [{
-      id: 1,
-      name: "example_name"
-    }];
+  describe("Create product - Model", async () => {
+    before(() => {
+      const execute = [{ insertId: 1 }];
 
-    sinon.stub(connection, "execute").resolves(execute);
-  });
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
 
-  after(() => {
-    connection.execute.restore();
-  });
+    after(() => {
+      connection.execute.restore();
+    });
 
-  it('should return a product with "id" and "name"', async () => {
-    const response = await productsModel.getProduct('1');
-    console.log(response);
+    it('should return a product "id" inserted', async () => {
+      const response = await productsModel.createProduct('name_example');
+      console.log(response);
 
-    expect(response).to.be.a('object');
-    expect(response).to.have.a.property('id');
-    expect(response).to.have.a.property('name');
+      expect(response).to.be.a('object');
+      expect(response).to.have.a.property('insertId');
+    });
   });
 });

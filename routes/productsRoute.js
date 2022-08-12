f055ff6a6c616e3b282d6b726fff47c774821667
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-12 12:00:12 -0300

    feat: create validations middleware for products

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productController = require('../controllers/productsController');
+const productsValidate = require('../middlewares/productsValidate');
 
 const route = express.Router();
 
 route.get('/', productController.getAllProducts);
 route.get('/:id', productController.getProduct);
-route.post('/', productController.createProduct);
+route.post('/', productsValidate, productController.createProduct);
 
 module.exports = route;

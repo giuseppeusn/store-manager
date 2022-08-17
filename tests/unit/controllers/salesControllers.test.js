385fbdc3ec9b75a8feebd96ea0f39fc21c60535a
Giuseppe <giuseppeuhlmann@gmail.com>
2022-08-17 16:40:15 -0300

    test: create controller test for PUT /sales/:id

--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -274,4 +274,80 @@ describe('Sales Controller tests', () => {
       });
     });
   });
+
+  describe('Update sale', () => {
+    describe('In case of success', () => {
+      const response = {};
+      const request = {};
+      const responseService = [
+        {
+          date: "2021-09-09T04:54:29.000Z",
+          productId: 1,
+          quantity: 2,
+        },
+        {
+          date: "2021-09-09T04:54:54.000Z",
+          productId: 2,
+          quantity: 2,
+        }
+      ];
+
+      const result = { code: 200, response: responseService };
+
+      before(() => {
+        request.body = responseService;
+        request.params = '1';
+
+        response.status = sinon.stub()
+          .returns(response);
+        response.json = sinon.stub().returns();
+        
+        sinon.stub(salesService, 'updateSale').resolves(result);
+      });
+
+      after(() => {
+        salesService.updateSale.restore();
+      });
+
+      it('should response with status 200', async () => {
+        await salesController.updateSale(request, response);
+
+        expect(response.status.calledWith(result.code)).to.be.equal(true);
+        expect(response.json.calledWith(result.response)).to.be.equal(true);
+      });
+    });
+
+    describe('In case of failing', () => {
+      const response = {};
+      const request = {};
+
+      const result = {
+        code: 404,
+        message: 'Product not found'
+      };
+
+      before(() => {
+        request.body = {};
+        request.params = '1';
+
+        response.status = sinon.stub()
+          .returns(response);
+        response.json = sinon.stub()
+          .returns();
+        
+        sinon.stub(salesService, 'updateSale').resolves(result);
+      });
+
+      after(() => {
+        salesService.updateSale.restore();
+      });
+
+      it('should response with status 404 and message "Product not found"', async () => {
+        await salesController.updateSale(request, response);
+
+        expect(response.status.calledWith(404)).to.be.equal(true);
+        expect(response.json.calledWith({ message: result.message })).to.be.equal(true);
+      });
+    });
+  });
 });
